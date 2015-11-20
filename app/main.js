85c3f4012337e57d093ebfc877947ebb1728dc3c
思維漫走 <silver_ras@163.com>
2015-11-20 21:43:15 +0800

    upload webpack.config.js

--- app/main.js
+++ app/main.js
@@ -3,6 +3,6 @@ import React      from 'react'
 import ReactDOM   from 'react-dom'
 import Datepicker from './components/datepicker.jsx'
 
-
 let node=document.getElementById('datepicker')
 ReactDOM.render(<Datepicker filter="mm dd, yyyy" />, node)
+ 
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,10 @@ var path = require('path');
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var webpack = require('webpack')
 var autoprefixer = require('autoprefixer');
-
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    devtool: 'inline-source-map',
+    devtool: 'source-map',
     entry: [
         'webpack/hot/dev-server',
         'webpack-dev-server/client?http://localhost:8080',
@@ -19,6 +19,14 @@ module.exports = {
         new webpack.HotModuleReplacementPlugin(),
         new ExtractTextPlugin('[name].[hash].css', {
             disable: true
+        }),
+        new webpack.NoErrorsPlugin(),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin(),
+        new HtmlWebpackPlugin({
+            template:'./app/index.html',
+            inject:'body',
+            minify:{}
         })
     ],
     module: {
@@ -27,9 +35,12 @@ module.exports = {
           loaders: ['react-hot', 'babel'],
           exclude: /node_modules/
         }, {
-          test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
+          test: /\.(woff|woff2|ttf|eot)$/,
           loader: 'file'
         }, {
+          test: /\.(png|jpg|jpeg|git|svg)$/,
+          loader:'url-loader?limit=8192'
+        },{
           test: /\.html$/,
           loader: 'raw'
         },{
